43319e4c9387f58b118b3747a49b0dbde34a1759
DjWarmonger <Warmonger@vp.pl>
2012-11-02 08:06:22 -0700

    Wrzuciłem program, który linkuje GSL, QT i Boost. Spróbuj go skompilować z tymi bibliotekami. Miłej zabawy ;)

---
+++ ZPR.cpp
@@ -0,0 +1,27 @@
+// ZPR.cpp : main project file.
+
+//include boost::foreach
+//include gsl fft
+//include qt?
+#include "stdafx.h"
+
+//GSL
+#include <gsl_math.h>
+#include <gsl\gsl_fft.h>
+//QT
+#include <Qt\qwidget.h>
+
+using namespace System;
+
+int main (array<System::String ^> ^args)
+{
+	std::vector<int> vec;
+	BOOST_FOREACH (auto element, vec)
+	{
+		element = 666;
+	}
+	QWidget widget;
+
+    Console::WriteLine (L"Hello World");
+    return 0;
+}
