935feaad376a6555d60233b0336fb2e9df573f6a
DjWarmonger <Warmonger@vp.pl>
2012-11-18 02:06:07 -0800

    Porządki w pliku głównym.

--- ZPR.cpp
+++ ZPR.cpp
@@ -1,27 +1,29 @@
 // ZPR.cpp : main project file.
 
-//include boost::foreach
-//include gsl fft
-//include qt?
 #include "stdafx.h"
 
 #include "Graph.h"
 #include "Analyzer.h"
 #include "ParserFactory.h"
 
-//GSL
-#include <gsl_math.h>
-#include <gsl\gsl_fft.h>
-//QT
-#include <Qt\qwidget.h>
+#include <boost/iostreams/stream.hpp>
+#include <boost/iostreams/stream_buffer.hpp>
+#include <boost/iostreams/device/file.hpp>
 
 using namespace System;
+namespace io = boost::iostreams;
 
-int main (array<System::String ^> ^args)
+int main (int argc, char** argv)
 {
 	try
 	{
 		ParserFactory::get().initParsers();
+
+		for (int i = 0; i < argc; ++i)
+		{
+			io::stream_buffer<io::file_source> s (argv[i]);
+		}
+
 		int bar;
 		ParserFactory::get().tryToParse(bar);
 	}
