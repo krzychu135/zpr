64dbb20799f15abc40bbd572546197cdd7ac6701
DjWarmonger <Warmonger@vp.pl>
2012-11-19 04:10:16 -0800

    Spirit, podejście drugie.

--- Parser.cpp
+++ Parser.cpp
@@ -5,36 +5,38 @@
 #include "boost\spirit\include\qi.hpp"
 #include <boost/spirit/include/phoenix_core.hpp>
 #include <boost/spirit/include/phoenix_operator.hpp>
+#include <boost/fusion/adapted/std_pair.hpp> //makes std::pair compatible with qi random acces sequence
+#include <boost/fusion/include/std_pair.hpp>
+#include <boost/fusion/include/io.hpp>
 
 namespace qi = boost::spirit::qi;
 namespace ascii = boost::spirit::ascii;
 
 using qi::double_;
+using qi::uint_;
+//using qi::val_;
 using boost::spirit::qi::_1;
 using qi::phrase_parse;
 using ascii::space;
+using ascii::space_type;
 using boost::phoenix::ref;
 
-typedef std::pair<double, double> range;
+typedef std::pair<unsigned int, unsigned int> range;
 
 template <typename Iterator>
-bool parseRange (Iterator first, Iterator last, range& limits)
+struct parseRange : qi::grammar <Iterator, range(), space_type>
 {
-	double first, second;
-    bool r = phrase_parse(first, last,
+	parseRange() : parseRanger::base_type (pair)
+	{
+		pair %=
+			uint_ >>
+			space_type >>
+			uint_ //two numbers separated by space
+		;
+	}
 
-        (
-			double_[ref(first) = _1] >>
-			space >>
-			double_[ref(second) = _1] //two numbers separated by space
-        ),
-        space); //pairs are delimited by another space
-
-    if (!r || first != last) // fail if we did not get a full match
-        return false;
-    limits = range(first, second);
-    return r;
-}
+	qi::rule<Iterator, range(), space_type> pair; //pairs are delimited by another space
+};
 
 void ParserSplice::getSequences(std::vector<Sequence> & out) const
 {
